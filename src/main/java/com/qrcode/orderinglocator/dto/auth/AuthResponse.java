2c30f28c075921749e933fc75a7c822722200b39
Ahmed Niazi <101074494+Zyrex24@users.noreply.github.com>
2025-08-20 14:10:49 +0300

    Add AuthResponse DTO for authentication responses

---
+++ src/main/java/com/qrcode/orderinglocator/dto/auth/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.qrcode.orderinglocator.dto.auth;
+
+import com.qrcode.orderinglocator.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private User.Role role;
+}
