3b04db9b953a3891db65e5b5e7e7764258485acf
Ahmed Niazi <101074494+Zyrex24@users.noreply.github.com>
2025-08-20 14:02:39 +0300

    Add main Spring Boot application class

---
+++ src/main/java/com/qrcode/orderinglocator/OrderingLocatorApplication.java
@@ -0,0 +1,13 @@
+package com.qrcode.orderinglocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderingLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderingLocatorApplication.class, args);
+    }
+
+}
