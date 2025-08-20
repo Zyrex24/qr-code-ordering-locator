df6f66b7b63e777fc7750da7720e428b2d705b97
Ahmed Niazi <101074494+Zyrex24@users.noreply.github.com>
2025-08-20 14:04:02 +0300

    Add Category entity for menu organization

---
+++ src/main/java/com/qrcode/orderinglocator/entity/Category.java
@@ -0,0 +1,28 @@
+package com.qrcode.orderinglocator.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Product> products;
+}
